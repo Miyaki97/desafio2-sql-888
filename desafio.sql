bd7adff68b9b27d6134a30a8c1e14fe0a73c6ad9
Hajime <millarayzunigag@gmail.com>
2024-04-16 16:36:09 -0400

    desafio arreglado

---
+++ desafio.sql
@@ -0,0 +1,64 @@
+drop table if exists inscritos;
+
+CREATE TABLE IF NOT EXISTS INSCRITOS(cantidad INT, fecha DATE, fuente
+VARCHAR);
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 44, '01/01/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 56, '01/01/2021', 'Página' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 39, '01/02/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 81, '01/02/2021', 'Página' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 12, '01/03/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 91, '01/03/2021', 'Página' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 48, '01/04/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 45, '01/04/2021', 'Página' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 55, '01/05/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 33, '01/05/2021', 'Página' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 18, '01/06/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 12, '01/06/2021', 'Página' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 34, '01/07/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 24, '01/07/2021', 'Página' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 83, '01/08/2021', 'Blog' );
+INSERT INTO INSCRITOS(cantidad, fecha, fuente)
+VALUES ( 99, '01/08/2021', 'Página' );
+
+select * from inscritos;
+
+
+--1
+SELECT COUNT(*) AS total_registros FROM INSCRITOS;
+
+
+--2
+SELECT SUM(cantidad) AS total_inscritos FROM INSCRITOS;
+
+--3
+SELECT *
+FROM INSCRITOS
+WHERE fecha = (SELECT MIN(fecha) FROM INSCRITOS);
+
+
+--4
+SELECT fecha, SUM(cantidad) AS total_inscritos
+FROM INSCRITOS
+GROUP BY fecha;
+
+--5
+SELECT fecha, SUM(cantidad) AS total_inscritos
+FROM INSCRITOS
+GROUP BY fecha
+ORDER BY total_inscritos DESC
+LIMIT 1;
